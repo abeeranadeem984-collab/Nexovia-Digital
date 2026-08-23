1e94fad0e91c7c82e9728a8128426d2204bb0717
abeeranadeem984-collab <abeeranadeem984@gmail.com>
2026-08-23 13:52:31 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/Nexovia-Digital/',
+    base: '/'
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
